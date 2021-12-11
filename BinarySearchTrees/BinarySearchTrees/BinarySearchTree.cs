64b45fec8ef11049e5ccc4783e597b2abd8109b1
Andrew Smith <andrewcs149@gmail.com>
2021-12-11 13:03:48 -0800

    Practice Binary Trees

--- BinarySearchTrees/BinarySearchTrees/BinarySearchTree.cs
+++ BinarySearchTrees/BinarySearchTrees/BinarySearchTree.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BinarySearchTrees
+﻿namespace BinarySearchTrees
 {
     internal class BinarySearchTree
     {
@@ -23,35 +17,25 @@ namespace BinarySearchTrees
 
         public void Remove(int value)
         {
-            if (Count == 0) return;
-            Remove(Root, value);
+            if (Count != 0)
+                Remove(Root, value);
         }
 
-        public Node Remove(Node root, int value)
+        private Node Remove(Node root, int value)
         {
             if (value < root.Value)
-            {
                 root.LeftChild = Remove(root.LeftChild, value);
-            }
             else if (value > root.Value)
-            {
                 root.RightChild = Remove(root.RightChild, value);
-            }
             else
             {
                 Count--;
                 if (root.LeftChild == null && root.RightChild == null)
-                {
                     root = null;
-                }
                 else if (root.LeftChild == null)
-                {
                     root = root.RightChild;
-                }
                 else if (root.RightChild == null)
-                {
                     root = root.LeftChild;
-                }
                 else
                 {
                     int max = Max(root.LeftChild);
@@ -63,83 +47,151 @@ namespace BinarySearchTrees
             return root;
         }
 
+        public void Insert(int value)
+        {
+            Node newNode = new Node(value);
+            if (Count == 0)
+            {
+                Count++;
+                Root = newNode;
+            }
+            else
+                Insert(Root, newNode);
+        }
+
+        private void Insert(Node root, Node newNode)
+        {
+            if (newNode.Value < root.Value)
+            {
+                if (root.LeftChild == null)
+                {
+                    Count++;
+                    root.LeftChild = newNode;
+                }
+                else
+                    Insert(root.LeftChild, newNode);
+            }
+            else if (newNode.Value > root.Value)
+            {
+                if (root.RightChild == null)
+                {
+                    Count++;
+                    root.RightChild = newNode;
+                }
+                else
+                    Insert(root.RightChild, newNode);
+            }
+            else
+                return;
+        }
+
         public void Insert(params int[] values)
         {
             Array.ForEach(values, val => Insert(val));
         }
 
-        public void Insert(int value)
+        public int Depth()
         {
-            Node newNode = new Node(value);
-            if (Count++ == 0)
+            if (Count == 0) return -1;
+
+            int depth = 0;
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(Root);
+            while (queue.Count > 0)
             {
-                Root = newNode;
+                Node tmp = queue.Dequeue();
+
+                if (tmp.LeftChild != null || tmp.RightChild != null)
+                    depth++;
+
+                if (tmp.LeftChild != null)
+                    queue.Enqueue(tmp.LeftChild);
+
+                if (tmp.RightChild != null)
+                    queue.Enqueue(tmp.RightChild);
             }
-            else
+
+            return depth;
+        }
+        public bool Contains(int value)
+        {
+            if (Count == 0) return false;
+
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(Root);
+            while (stack.Count > 0)
             {
-                Insert(Root, newNode);
+                Node tmp = stack.Pop();
+
+                if (tmp.Value == value)
+                    return true;
+
+                if (tmp.RightChild != null)
+                    stack.Push(tmp.RightChild);
+
+                if (tmp.LeftChild != null)
+                    stack.Push(tmp.LeftChild);
             }
+
+            return false;
         }
 
-        public void Insert(Node root, Node newNode)
+        public void PrintLevelOrder()
         {
-            if (newNode.Value == root.Value) return;
+            if (Count == 0) return;
 
-            if (newNode.Value < root.Value)
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(Root);
+            while (queue.Count > 0)
             {
-                if (root.LeftChild == null)
-                    root.LeftChild = newNode;
-                else
-                    Insert(root.LeftChild, newNode);
+                Node tmp = queue.Dequeue();
+                Console.Write(tmp.Value + " - ");
+
+                if (tmp.LeftChild != null)
+                    queue.Enqueue(tmp.LeftChild);
+
+                if (tmp.RightChild != null)
+                    queue.Enqueue(tmp.RightChild);
             }
-            else
-            {
-                if (root.RightChild == null)
-                    root.RightChild = newNode;
-                else
-                    Insert(root.RightChild, newNode);
-            }
+
+            Console.WriteLine();
         }
+
+
 
         public void PrintInOrderRecursive()
         {
-            if (Count == 0) return;
-            PrintInOrderRecursive(Root);
+            if (Count != 0)
+                PrintInOrderRecursive(Root);
+
             Console.WriteLine();
         }
-        public void PrintInOrderRecursive(Node root)
+        private void PrintInOrderRecursive(Node root)
         {
             if (root.LeftChild != null)
-            {
                 PrintInOrderRecursive(root.LeftChild);
-            }
 
             Console.Write(root.Value + " - ");
 
             if (root.RightChild != null)
-            {
                 PrintInOrderRecursive(root.RightChild);
-            }
         }
 
         public void PrintPostOrderRecursive()
         {
-            if (Count == 0) return;
+            if (Count != 0)
+                PrintPostOrderRecursive(Root);
 
-            PrintPostOrderRecursive(Root);
             Console.WriteLine();
         }
         private void PrintPostOrderRecursive(Node root)
         {
             if (root.LeftChild != null)
-            {
                 PrintPostOrderRecursive(root.LeftChild);
-            }
 
             if (root.RightChild != null)
-            {
                 PrintPostOrderRecursive(root.RightChild);
-            }
 
             Console.Write(root.Value + " - ");
         }
@@ -147,9 +199,9 @@ namespace BinarySearchTrees
 
         public void PrintPreOrderRecursive()
         {
-            if (Count == 0) return;
+            if (Count != 0)
+                PrintPreOrderRecursive(Root);
 
-            PrintPreOrderRecursive(Root);
             Console.WriteLine();
         }
         private void PrintPreOrderRecursive(Node root)
@@ -157,18 +209,13 @@ namespace BinarySearchTrees
             Console.Write(root.Value + " - ");
 
             if (root.LeftChild != null)
-            {
                 PrintPreOrderRecursive(root.LeftChild);
-            }
 
             if (root.RightChild != null)
-            {
                 PrintPreOrderRecursive(root.RightChild);
-            }
         }
 
-
-        public void PrintInOrderIterative()
+        public void PrintPreOrderIterative()
         {
             if (Count == 0) return;
 
@@ -177,17 +224,13 @@ namespace BinarySearchTrees
             while (stack.Count > 0)
             {
                 Node tmp = stack.Pop();
+                Console.Write(tmp.Value + " - ");
+
                 if (tmp.RightChild != null)
-                {
                     stack.Push(tmp.RightChild);
-                }
 
                 if (tmp.LeftChild != null)
-                {
                     stack.Push(tmp.LeftChild);
-                }
-
-                Console.Write(tmp.Value + " - ");
             }
 
             Console.WriteLine();
--- BinarySearchTrees/BinarySearchTrees/Program.cs
+++ BinarySearchTrees/BinarySearchTrees/Program.cs
@@ -5,8 +5,9 @@
         private static void Main(string[] args)
         {
             BinarySearchTree bst = new BinarySearchTree();
-            bst.Insert(5, 3, 6, 2, 4);
-            bst.PrintInOrderIterative();
+            bst.Insert(5, 3, 6, 2, 4, 7, 0);
+            bst.PrintPostOrderRecursive();
+            Console.WriteLine(bst.Depth());
         }
     }
 }
